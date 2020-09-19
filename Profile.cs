a3694d47a873d278c2c0f318592196994057d3a3
Joke Heyndels <jo_hannah@outlook.com>
2020-09-19 11:23:39 +0200

    End of pre-determined assignment

---
+++ Profile.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace DatingProfile
+{
+    class Profile
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+       public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+       {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[5];
+       }
+
+       public string ViewProfile()
+        {
+            string hobbiesString = String.Join(", ", hobbies);
+            return $"{name}, {age}\nFrom {city}, {country}\nPronouns: {pronouns}\nHobbies: {hobbiesString}";
+        }
+        
+       public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace ooya
+namespace DatingProfile
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Profile sam = new Profile("Sam Drakkilla", 30, "New York", "USA", "he/him");
+            Console.WriteLine(sam.ViewProfile());
+            sam.SetHobbies(new string[] {"listening to audiobooks and podcasts", "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice columns"});
+            Console.WriteLine(sam.ViewProfile());
         }
     }
 }
